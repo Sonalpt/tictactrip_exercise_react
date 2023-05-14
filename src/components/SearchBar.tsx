05d230ddbe24b0c93bc4ec578322d53eea8a59d4
Sonalpt <remsbdj@gmail.com>
2023-05-14 19:56:29 +0200

    repository init, with base style for mobile

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import '../styles/index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCircleDot, faChevronDown, faChevronUp, faArrowsUpDown, faLocationDot, faCalendar } from '@fortawesome/free-solid-svg-icons';
+
+export const SearchBar = () => {
+  return (
+    <nav>
+      <div className='container'>
+        <div className='trip_details'>
+        <div className='trip_type'>
+            <span>One-way</span>
+            <FontAwesomeIcon icon={faChevronDown} />
+          </div>
+          <div className='passengers_details'>
+            <span>1 adult, No discount card</span>
+            <FontAwesomeIcon icon={faChevronDown} />
+          </div>
+        </div>
+        <div className='trip_from'>
+          <FontAwesomeIcon icon={faCircleDot} />
+          <input type="text" />
+          <FontAwesomeIcon icon={faArrowsUpDown} />
+        </div>
+        <div className="trip_to">
+          <FontAwesomeIcon icon={faLocationDot} />
+          <input type="text" />
+        </div>
+        <div className="date">
+          <div className="date_from">
+            <FontAwesomeIcon icon={faCalendar} />
+            <input type="text" placeholder='+Add departure'/>
+          </div>
+          <div className="separation_bar"></div>
+          <div className="date_to">
+            <FontAwesomeIcon icon={faCalendar} />
+            <input type="text" placeholder='+Add return'/>
+          </div>
+        </div>
+        <button>Search</button>
+        <div className="toggle_accomodation">
+          <div className="toggle_button">
+            <div className="toggle_circle"></div>
+          </div>
+          <span>Find my accomodation</span>
+        </div>
+      </div>
+    </nav>
+  )
+}
---
+++ src/views/Home.tsx
@@ -0,0 +1,12 @@
+import { SearchBar } from '../components/SearchBar'
+
+export const Home = () => {
+  return (
+    <>
+        <div className='home'>
+        <SearchBar />
+        </div>
+        
+    </>
+  )
+}
